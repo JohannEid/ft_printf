5236ed59466d457b812274a606208febcdb1dd46
JohannEid <jojoking97@gmail.com  git config --global user.name SOATRAP git commit -m Add>
2017-02-04 12:25:25 +0100

    beginning new project to code printf function

---
+++ cmake-build-debug/CMakeFiles/printf_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/printf_project.dir/main.c.o"
+  "printf_project.pdb"
+  "printf_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/printf_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
