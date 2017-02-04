29d526c819433564b6300e98e50cf34a7b4a76c2
JohannEid <jojoking97@gmail.com  git config --global user.name SOATRAP git commit -m Add>
2017-02-04 13:32:11 +0100

    going to implement dynamic array to get all the %d %f ... expressions there are in the string

--- misc.c
+++ misc.c
@@ -2,26 +2,39 @@
 // Created by johann on 04/02/17.
 //
 
+#include <zconf.h>
 #include "misc.h"
 
 
-void ft_printf(const char* to_print,...) {
-    char error_msg [] = "There was an error in the output of the function";
+void ft_printf(const char *to_print, ...) {
 
-//    search_for_expression(to_print);
-    if (write(1, to_print , size_of_string(to_print)) != size_of_string(to_print)){
-        write(2, error_msg,size_of_string(error_msg));
+    char error_msg[] = "There was an error in the output of the function";
+    size_t size_of_print = size_of_string(to_print);
+    size_t size_of_error = size_of_string(error_msg);
+    va_list list_of_variables;
+    va_start(list_of_variables, to_print);
+
+
+    search_for_expression(to_print);
+    if (write(1, to_print, size_of_print) != size_of_print) {
+        write(2, error_msg, size_of_error);
     }
 
 }
 
- const size_t  size_of_string(const char * to_size) {
-     size_t i = 0;
-    while (to_size[i] != '\0')
-    {
-        ++i;
+const size_t size_of_string(const char *to_size) {
+    size_t counter = 0;
+    while (to_size[counter] != '\0') {
+        ++counter;
     }
-    return i;
+    return counter;
 }
 
-//void search_for_expression()
+
+void search_for_expression(const char *to_search) {
+    int counter = 0;
+    while
+
+
+
+}
